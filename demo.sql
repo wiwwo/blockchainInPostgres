2bba34f318e727232b2ad9e98805e44c32180bb1
Wiwwo <wiwwo+github@wiwwo.com>
2018-02-09 12:37:08 +0100

    Added bitcoinISH demo

--- demo.sql
+++ demo.sql
@@ -26,7 +26,7 @@ commit;
 select * from blockchainInPostgres.events order by eventEpoch;
 \echo
 \echo and at blockchain table
-select * from blockchainInPostgres.blockChain order by blockepoch;
+select * from blockchainInPostgres.blockChain order by blockEpoch;
 
 
 \echo
@@ -44,7 +44,7 @@ update blockchainInPostgres.events set info1='xxx';
 select * from blockchainInPostgres.events order by eventEpoch;
 \echo
 select blockchainInPostgres.generateBlock() as "NowGeneratingBlock";
-select * from blockchainInPostgres.blockChain order by blockepoch;
+select * from blockchainInPostgres.blockChain order by blockEpoch;
 
 \echo
 
@@ -52,7 +52,7 @@ select blockchainInPostgres.generateBlock() as "NowGeneratingBlock";
 
 \echo ----------------------
 --select * from blockchainInPostgres.events order by eventEpoch;
---select * from blockchainInPostgres.blockChain order by blockepoch;
+--select * from blockchainInPostgres.blockChain order by blockEpoch;
 
 select blockchainInPostgres.validateBlock() as "BlockValidationResult";
 
@@ -78,7 +78,7 @@ select blockchainInPostgres.validateBlock() as "BlockValidationResult";
 \echo
 select * from blockchainInPostgres.events order by eventEpoch;
 \echo
-select * from blockchainInPostgres.blockChain order by blockepoch;
+select * from blockchainInPostgres.blockChain order by blockEpoch;
 \echo
 \echo
 
@@ -89,3 +89,40 @@ update blockchainInPostgres.blockChain set blockEpoch = 1111111111 where blockHe
 select * from blockchainInPostgres.blockChain where blockHeight = 2;
 select blockchainInPostgres.validateBlock() as "BlockValidationResult";
 
+
+\echo
+\echo
+\echo Bitcoin blockchain example
+alter table blockchainInPostgres.blockChain disable trigger readOnlyBlockChain;
+alter table blockchainInPostgres.events     disable trigger readOnlyEvent;
+delete from blockchainInPostgres.events;
+delete from blockchainInPostgres.blockChain;
+alter table blockchainInPostgres.blockChain enable trigger readOnlyBlockChain;
+alter table blockchainInPostgres.events     enable trigger readOnlyEvent;
+alter table blockchainInPostgres.events     enable trigger onEventInsert;
+
+drop sequence if exists blockchainInPostgres.blockHeightSeq;
+create sequence blockchainInPostgres.blockHeightSeq start 1;
+insert into blockchainInPostgres.blockChain (blockHeight, blockEpoch, eventsHash, nonce, blockHash)
+values (0, 0, 0, 0, 'genesis');
+
+
+\echo
+insert into blockchainInPostgres.events (info1, info2, info3)
+values ('FROM GENESIS','TO RobinHood y8c0bbd7ecc77a0924dadce65a09b2ead146ad15', '999 BTC')
+     , ('FROM RobinHood xb7a04e27dcbcfe0cedf06faa4f36c084d4a1a29','TO PoorPerson1 y8c0bbd7ecc77a0924dadce65a09b2ead146ad15', '99 BTC')
+     , ('FROM RobinHood xb7a04e27dcbcfe0cedf06faa4f36c084d4a1a29','TO PoorPerson2 efb71bb90b344a3495adf3457a8705c178beeb03', '99 BTC')
+     , ('FROM RobinHood xb7a04e27dcbcfe0cedf06faa4f36c084d4a1a29','TO PoorPerson3 161897cffdbd367faedc1484f705788001acdbbb', '99 BTC')
+     , ('FROM PoorPerson3 161897cffdbd367faedc1484f705788001acdbbb','TO PoorPerson4 27142e2859f95012f45951e47621bfd6153af46e', '9 BTC')
+     , ('FROM PoorPerson3 161897cffdbd367faedc1484f705788001acdbbb','TO PoorPerson5 c40caa2ac5de6957097512e61bf0d590c3b7446d', '9 BTC')
+;
+
+select * from blockchainInPostgres.events order by eventEpoch;
+select blockchainInPostgres.generateBlock() as "NowGeneratingBlock";
+select blockchainInPostgres.generateBlock() as "NowGeneratingBlock";
+select blockchainInPostgres.generateBlock() as "NowGeneratingBlock";
+select blockchainInPostgres.generateBlock() as "NowGeneratingBlock";
+select blockchainInPostgres.generateBlock() as "NowGeneratingBlock";
+select * from blockchainInPostgres.events order by eventEpoch;
+\echo
+select * from blockchainInPostgres.blockChain order by blockEpoch;
